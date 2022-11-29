ab0f7714426f7c45b16d670ab23a855fd62a24c3
Atlantik <atlantiklimani1@gmail.com>
2022-11-29 21:39:11 +0100

    getEncrypted Password

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -20,4 +20,18 @@ public class JavaApplication2 {
 
     }
 
+    public static byte[] getEncryptedPassword(String password, byte[] salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // PBKDF2 with SHA-1 as the hashing algorithm. Note that the NIST
+        // specifically names SHA-1 as an acceptable hashing algorithm for PBKDF2
+        String algorithm = "PBKDF2WithHmacSHA1";
+        // SHA-1 generates 160 bit hashes, so that's what makes sense here
+        int derivedKeyLength = 512;
+        // Pick an iteration count that works for you. The NIST recommends at
+        // least 1,000 iterations:
+        // http://csrc.nist.gov/publications/nistpubs/800-132/nist-sp800-132.pdf
+        // iOS 4.x reportedly uses 10,000:
+        // http://blog.crackpassword.com/2010/09/smartphone-forensics-cracking-blackberry-backup-passwords/
+    }
+
 }
