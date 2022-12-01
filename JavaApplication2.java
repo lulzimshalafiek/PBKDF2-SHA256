6469480f9aec08520b84730df63e88def9fb1ac2
lulzimshalafiek <119074090+lulzimshalafiek@users.noreply.github.com>
2022-12-01 11:35:46 +0100

    Update JavaApplication2.java

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -9,23 +9,20 @@ import javax.crypto.spec.PBEKeySpec;
 
 
 public class JavaApplication2 {
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) throws Exception {
-        // TODO code application logic here
+      
 
     }
 
     public static byte[] getEncryptedPassword(String password, byte[] salt)
             throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // PBKDF2 with SHA-1 as the hashing algorithm. Note that the NIST
-        // specifically names SHA-1 as an acceptable hashing algorithm for PBKDF2
+        
+        
         String algorithm = "PBKDF2WithHmacSHA1";
-        // SHA-1 generates 160 bit hashes, so that's what makes sense here
+       
         int derivedKeyLength = 512;
-        // Pick an iteration count that works for you. The NIST recommends at
-        // least 1,000 iterations:
+       
         
         int iterations = 4096;
 
