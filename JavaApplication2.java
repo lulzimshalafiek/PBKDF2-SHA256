5dff8e486aff0103cbb75b1fbbcd4d6faf4eb27a
GenitiH <114908049+GenitiH@users.noreply.github.com>
2022-12-02 17:43:41 +0100

    Authenticate

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -15,6 +15,17 @@ public class JavaApplication2 {
 
     }
 
+    public static boolean authenticate(String attemptedPassword, byte[] encryptedPassword, byte[] salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // Encrypt the clear-text password using the same salt that was used to
+        // encrypt the original password
+        byte[] encryptedAttemptedPassword = getEncryptedPassword(attemptedPassword, salt);
+
+        // Authentication succeeds if encrypted password that the user entered
+        // is equal to the stored hash
+        return Arrays.equals(encryptedPassword, encryptedAttemptedPassword);
+    }
+
     public static byte[] getEncryptedPassword(String password, byte[] salt)
             throws NoSuchAlgorithmException, InvalidKeySpecException {
         
